b1b5e67e78b92606cafc641ce6a9e7cd1465106d
Anton Sekatski <antonsekatski@gmail.com>
2017-01-19 11:28:59 +0300

    fix something

--- src/actions/messages.js
+++ src/actions/messages.js
@@ -1,5 +1,3 @@
-// I know, Arturs wrote that there is no need for chat rooms. I just wanted to show how ridiculously easy this is when we have Key -> Value store (Beeblebrox library) instead of Nested Tree Store (Redux). We can use Redux in the flat key-value way but it doesn't make it any difference because we still need to create actions and reducers. 
-// Look at this sh*t - simple and easy as heck: 
 export function send(body) {
   const socket = this.get('socket')
   const user = this.get('current_user')
